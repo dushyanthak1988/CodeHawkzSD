1f59e5370876c978b3280617f3e8c36d4c96c9af
Dushyantha Kalehewatta <Ulindu.Dushyantha@softlogiclife.lk>
2022-03-14 18:55:14 +0530

    add Student Request model  validation

--- Source/CodingChallenge.SeniorDev.V1.Common/DTO/StudentCreateModel.cs
+++ Source/CodingChallenge.SeniorDev.V1.Common/DTO/StudentCreateModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,22 @@ namespace CodingChallenge.SeniorDev.V1.Common.DTO
 {
     public class StudentCreateModel
     {
+        [RegularExpression(@"^[ST]+\d{0,3}$")]
+        [Required]
         public string RegistrationID { get; set; }
+        [StringLength(30, MinimumLength = 2)]
+        [Required]
         public string FirstName { get; set; }
+        [StringLength(30, MinimumLength = 2)]
+        [Required]
         public string LastName { get; set; }
         public DateTimeOffset Birthdate { get; set; }
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
+
+        [RegularExpression(@"^([0-9]{9}[x|X|v|V]|[0-9]{12})$")]
+        [Required]
         public string NICNo { get; set; }
 
     }
